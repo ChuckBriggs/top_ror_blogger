415f4f37878f0ef4eb50f9a7a531cc0e0aaa5d77
Charles Briggs <chucklistmail+site.github.com@gmail.com>
2020-11-20 15:01:13 -0700

    Tagging feature completed

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,20 @@
+class TagsController < ApplicationController
+  include TagsHelper
+
+  before_action :require_login, only: [:destroy]
+
+  def show
+    @tag = Tag.find(params[:id])
+  end
+
+  def index
+    @tags = Tag.all
+  end
+
+  # def destroy
+  #   @tag = Tag.find(params[:id])
+  #   @tag.destroy
+  #   flash.notice = "Tag '#{@tag.name}' Deleted!"
+  #   redirect_to articles_path
+  # end
+end
